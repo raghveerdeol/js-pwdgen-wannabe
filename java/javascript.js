0db403223644f656059826554f301c2816711c7a
raghveerdeol <deolraghveer5@gmail.com>
2024-05-07 17:10:25 +0200

    final

---
+++ java/javascript.js
@@ -0,0 +1,6 @@
+let nome = prompt('inserisci nome');
+let cognome = prompt('inserisci cognome');
+let colore = prompt('inserisci colore preferito');
+let numero = 23
+
+document.getElementById("show").innerHTML = (nome + cognome + colore + numero)
